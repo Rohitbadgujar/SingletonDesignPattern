f9e057edb0e3bac85046c873d258c785fcf1bec8
Rohit Badgujar <rohit.badgujar>
2022-09-08 22:54:08 +0100

    Singleton Design Pattern Implementation C#

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //The getInstance method is used also to provide a global point of
+            //access to the object and it can be used in the following manner:
+            Singleton.getInstance().Method("First Call");
+
+
+            //Real world Example of Singleton Pattern
+            //1. Logger Class
+            //2. Configuration Class
+
+        }
+    }
+}
---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Singleton
+    {
+
+        public static Singleton instance;
+        
+        //The constructor should not be accessible from the outside of the class
+        private Singleton() { 
+        
+        }
+        public static Singleton getInstance() {
+            //getInstance method ensures that only one instance of the class is created.
+            if (instance == null)
+                instance = new Singleton();
+
+            return instance;
+        }
+
+        public void Method(string str)
+        {
+            Console.WriteLine("Calling from {0}", str);
+        }
+
+    }
+}
